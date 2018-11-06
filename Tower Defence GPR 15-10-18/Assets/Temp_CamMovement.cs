b1426fca207a7b6589f456ce6aba58c8fce0ea4b
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2018-11-06 09:17:38 +0100

    Improved Minimap

--- Tower Defence GPR 15-10-18/Assets/Temp_CamMovement.cs
+++ Tower Defence GPR 15-10-18/Assets/Temp_CamMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Temp_CamMovement : MonoBehaviour {
     Transform camT;
-    float camMovementSpeed = 3.8f;
+    [SerializeField]
+    private float camMovementSpeed = 3.8f;
 	// Use this for initialization
 	void Start () {
         camT = GetComponent<Transform>();
@@ -17,22 +18,19 @@ public class Temp_CamMovement : MonoBehaviour {
 
     private void CamMovement()
     {
-         if (camT.transform.position.y < -4.522024f)
-         {
-        if (Input.GetKey(KeyCode.W))
-            {
-                camT.transform.Translate(0, camMovementSpeed * Time.deltaTime, 0);
-            }
+        if (Input.GetKey(KeyCode.W) && camT.transform.position.y < -4.522024f)
+        {
+            camT.transform.Translate(0, camMovementSpeed * Time.deltaTime, 0);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && camT.transform.position.x > 8.3715)
         {
             camT.transform.Translate(-camMovementSpeed * 1.5f * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && camT.transform.position.y > -10.58793)
         {
             camT.transform.Translate(0, -camMovementSpeed * Time.deltaTime, 0);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && camT.transform.position.x < 27.63964f)
         {
             camT.transform.Translate(camMovementSpeed * 1.5f * Time.deltaTime, 0, 0);
         }
